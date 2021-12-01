9e2f3bcc8cbddbdc4e9385db9050587b059be5a5
Denitsa Pesheva <dpessheva>
2021-12-01 11:52:06 +0100

    refactor formlogin component & added action for handlecSubmit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,29 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-export default function LoginForm ({state, onSubmit}) {
+export default function LoginForm ({profile:{email,password}, handleSubmit}) {
     return (
     
-        <form className='form' style={{ display: 'flex', margin: '5em' }}>
+        <form className='form' style={{ display: 'flex', margin: '5em' }} onClick={handleSubmit}>
             <div style={{display:'flex', margin:'1em' }}>
                  <label htmlFor='email' className='form-label' style={{marginLeft:'1em', padding:'1em'}}>Email</label>
-                <input type='email' value={state.email} id='email' style={{ borderRadius: '1em', padding:'1em' }}/>
+                <input type='email' value={email} id='email' style={{ borderRadius: '1em', padding:'1em' }}/>
             </div>
             <div style={{display:'flex', margin:'1em' }}>
                  <label htmlFor='password' style={{marginLeft:'1em', padding:'1em'}}>Password</label>
-                <input type='password' value={state.password} id='password' style={{ borderRadius: '1em',padding:'1em' }}/>
+                <input type='password' value={password} id='password' style={{ borderRadius: '1em',padding:'1em' }}/>
             </div>
-            <button type='submit' style={{ borderRadius:'1em', paddingLeft:'3em', paddingRight:'3em', margin:'1em' }} onSubmit={onSubmit}>Submit</button>
+            <button type='button' style={{ borderRadius:'1em', paddingLeft:'3em', paddingRight:'3em', margin:'1em' }} >Submit</button>
             </form>
     )
 }
 
 LoginForm.prototype = {
-    state: {
+    profile:PropTypes.shape({
         email: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired, 
-    },
-   
-    onSubmit: PropTypes.func,
+         password: PropTypes.string.isRequired, 
+         handleSubmit:PropTypes.func
+    })
+    
    
 }
--- src/components/LoginForm.stories.js
+++ src/components/LoginForm.stories.js
@@ -4,15 +4,15 @@ import { within, userEvent } from '@storybook/testing-library';
 
 export default {
   component: LoginForm,
-    title: 'LoginForm',
-    onClick: { action: 'clicked' },
+  title: 'LoginForm',
+  argTypes: { handleSubmit: { action: 'handleSubmit' } },
     parameters: {
     docs: {
       source: {
         type: 'code',
       },
-      onClick: { action: 'clicked' },
-    },
+     
+      }
   },
 };
 
@@ -20,32 +20,28 @@ const Template = args => <LoginForm {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    state: {
-     email: '',
-    password:''
-   }
+    profile: {}
 }
 
 export const FilledForm = Template.bind({});
 FilledForm.args = {
-    state: {
+    profile: {
      email: 'email@provider.com',
     password: 'a-random-password',
-  }
-    
-    
+  },
 }
-FilledForm.play = async ({ canvasElement }) => {
+// FilledForm.play = async ({ canvasElement }) => {
   // Starts querying the component from it's root element
-  const canvas = within(canvasElement);
+  // const canvas = within(canvasElement);
+  // console.log(canvasElement);
 
-//   await userEvent.type(canvas.queryByTestId('email'), 'email@provider.com', {
-//     delay: 100,
-//   });
-//   await userEvent.type(canvas.queryByTestId('password'), 'a-random-password', {
-//     delay: 100,
-//   });
+  // await userEvent.type(canvas.queryByTestId('email'), 'email@provider.com', {
+  //   delay: 100,
+  // });
+  // await userEvent.type(canvas.queryByTestId('password'), 'a-random-password', {
+  //   delay: 100,
+  // });
 
   // See https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
-//   await userEvent.click(canvas.getByRole('submit'));
-};
+  // await userEvent.click(canvas.getByRole('submit'));
+// };
