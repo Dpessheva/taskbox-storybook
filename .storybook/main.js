fd92c055c2623512ec9111c9ee08bc75db8d3a63
Denitsa Pesheva <dpessheva>
2021-12-01 13:09:48 +0100

    updated main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,17 +12,17 @@ module.exports = {
   },
 }
 
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'light',
-    toolbar: {
-      icon: 'circlehollow',
-      // Array of plain string values or MenuItem shape (see below)
-      items: ['light', 'dark'],
-      // Property that specifies if the name of the item will be displayed
-      showName: true,
-    },
-  },
-};
+// export const globalTypes = {
+//   theme: {
+//     name: 'Theme',
+//     description: 'Global theme for components',
+//     defaultValue: 'light',
+//     toolbar: {
+//       icon: 'circlehollow',
+//       // Array of plain string values or MenuItem shape (see below)
+//       items: ['light', 'dark'],
+//       // Property that specifies if the name of the item will be displayed
+//       showName: true,
+//     },
+//   },
+// };
